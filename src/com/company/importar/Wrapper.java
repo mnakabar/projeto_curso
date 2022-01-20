761ca31cd7874897b1dd39a034a9d0fc2acd7a34
“mnakabar” <mauricio.nakabar@mercadolivre.com>
2022-01-20 15:05:06 -0300

    criar branch nova

---
+++ src/com/company/importar/Wrapper.java
@@ -0,0 +1,23 @@
+package com.company.importar;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        // No Java temos para cada versão tipo primitivo uma versão equivalente OO
+
+        Byte b = 10; // byte
+        Short s = 100; // short
+        Integer i = 1000; // int
+        Long l = 123000L; // long
+        Float f = 1000.10F; // float
+        Double d = 1234.098; // double
+
+        Character c = 'A'; // char
+        Boolean bo = false || true; // boolean
+
+        System.out.println(b + " / " + s + " / " + i + " / "
+        + l + " / " + f + " / " + d + " / " + c + " / " +bo);
+    }
+
+}
